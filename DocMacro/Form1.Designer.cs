e7a69070a94219b31c3c022864629a65636a7085
Bryan Quinalayo <bryanaldrin.ph1@gmail.com>
2017-10-02 13:05:55 +0800

    Add project files.

---
+++ DocMacro/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace DocMacro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openPDFbutton = new System.Windows.Forms.Button();
+            this.ofd1 = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // openPDFbutton
+            // 
+            this.openPDFbutton.Location = new System.Drawing.Point(12, 12);
+            this.openPDFbutton.Name = "openPDFbutton";
+            this.openPDFbutton.Size = new System.Drawing.Size(179, 58);
+            this.openPDFbutton.TabIndex = 0;
+            this.openPDFbutton.Text = "Select PDF";
+            this.openPDFbutton.UseVisualStyleBackColor = true;
+            this.openPDFbutton.Click += new System.EventHandler(this.openPDFbutton_Click);
+            // 
+            // ofd1
+            // 
+            this.ofd1.FileName = "pdf";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 208);
+            this.Controls.Add(this.openPDFbutton);
+            this.Name = "Form1";
+            this.Text = "DocMacro";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openPDFbutton;
+        private System.Windows.Forms.OpenFileDialog ofd1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ DocMacro/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace DocMacro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openPDFbutton_Click(object sender, EventArgs e)
+        {
+            ofd1.Filter = "PDF Files | *.pdf";
+            if(ofd1.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(GetTextFromPDF(ofd1.FileName));
+            }
+        }
+
+        private string GetTextFromPDF(string path)
+        {
+            StringBuilder text = new StringBuilder();
+            using (PdfReader reader = new PdfReader(path))
+            {
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+            }
+
+            return text.ToString();
+        }
+    }
+}
